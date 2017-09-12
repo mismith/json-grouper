617b6f153cd1bc85a0ee14336505b921753380f8
Murray Smith <murray.smith@renoworks.com>
2017-09-12 14:20:16 -0600

    add file uploading / button

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,13 @@
     <md-toolbar class="md-dense">
       <h1 class="md-title">JSON Grouper</h1>
       <span style="flex-grow: 1"></span>
+
+      <md-button class="md-icon-button file-upload-button">
+        <md-icon>file_upload</md-icon>
+        <input type="file" accept="text/plain,application/json" @change="handleFileUpload($event.target.files[0])" @drop="handleFileUpload($event.dataTransfer.files[0])" />
+        <md-tooltip>Upload File</md-tooltip>
+      </md-button>
+
       <md-button @click.native="EventBus.$emit('group.collapseAll')" class="md-icon-button">
         <md-icon>remove_circle</md-icon>
         <md-tooltip>Collapse All</md-tooltip>
@@ -11,6 +18,7 @@
         <md-icon>add_circle</md-icon>
         <md-tooltip>Expand All</md-tooltip>
       </md-button>
+
       <md-button @click.native="EventBus.$emit('export-json')" class="md-icon-button">
         <md-icon>file_download</md-icon>
         <md-tooltip>Export JSON</md-tooltip>
@@ -35,38 +43,67 @@ export default {
     };
   },
   methods: {
-    loadText(url) {
+    handleFileUpload(file) {
+      this.data = undefined;
+
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const content = e.target.result;
+        switch (file.type) {
+          case 'application/json':
+            this.parseJson(JSON.parse(content));
+            break;
+          case 'text/plain':
+          default:
+            this.parseText(content);
+            break;
+        }
+      };
+      reader.readAsText(file);
+    },
+
+    fetchText(url) {
       return fetch(url)
-        .then(res => res.text())
-        .then((text) => {
-          const data = [];
-          const lines = text.replace(/^\s+|\s+$/ig, '').split('\n');
-          lines.forEach((line, i) => {
-            data.push({
-              $id: i,
-              name: line.replace(/^\s+|\s+$/ig, ''),
-              votes: 1,
-            });
-          });
-          this.data = {
-            data,
-          };
+        .then(res => res.text());
+    },
+    fetchJson(url) {
+      return fetch(url)
+        .then(res => res.json());
+    },
+
+    parseText(text) {
+      const data = [];
+      const lines = text.replace(/^\s+|\s+$/ig, '').split('\n');
+      lines.forEach((line, i) => {
+        data.push({
+          $id: i,
+          name: line.replace(/^\s+|\s+$/ig, ''),
+          votes: 1,
         });
+      });
+      this.data = {
+        data,
+      };
+    },
+    parseJson(json) {
+      let data = {};
+      if (json.data) {
+        data = json;
+      } else {
+        data = {
+          data: json,
+        };
+      }
+      this.data = data;
+    },
+
+    loadText(url) {
+      return this.fetchText(url)
+        .then(this.parseText);
     },
     loadJson(url) {
-      return fetch(url)
-        .then(res => res.json())
-        .then((json) => {
-          let data = {};
-          if (json.data) {
-            data = json;
-          } else {
-            data = {
-              data: json,
-            };
-          }
-          this.data = data;
-        });
+      return this.fetchJson(url)
+        .then(this.parseJson);
     },
   },
   created() {
@@ -78,7 +115,7 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 html,
 body,
 #app {
@@ -88,5 +125,17 @@ body,
 #app {
   display: flex;
   flex-direction: column;
+}
+
+.file-upload-button {
+  position: relative;
+
+  input[type=file] {
+    position: absolute;
+    top: 0;
+    left: 0;
+    font-size: 1000px;
+    opacity: 0;
+  }
 }
 </style>
