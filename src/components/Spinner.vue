88085ff7765bb8c7a794e54cd48a4c79931370ae
Murray Smith <murray.smith@renoworks.com>
2017-09-15 11:46:33 -0600

    add custom md-spinner component

---
+++ src/components/Spinner.vue
@@ -0,0 +1,16 @@
+<template>
+  <md-button class="md-icon-button md-spinner">
+    <md-icon>more_vert</md-icon>
+  </md-button>
+</template>
+
+<style scoped>
+@keyframes spin {
+  0% { transform: rotate(0); }
+  100% { transform: rotate(360deg); }
+}
+.md-spinner {
+  animation: spin 1s infinite linear;
+  margin: 0;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import VueMaterial from 'vue-material';
 import 'vue-material/dist/vue-material.css';
+import Spinner from '@/components/Spinner';
 
 import App from './App';
 
 Vue.prototype.EventBus = new Vue();
 Vue.config.productionTip = false;
 Vue.use(VueMaterial);
+Vue.component('md-spinner', Spinner);
 
 /* eslint-disable no-new */
 new Vue({
